e6ec569ad56e24294ea454ddaa3ab968d67da250
Sara Hincapie <shincap8@gmail.com>
2020-06-16 15:49:52 -0500

    Add something found on web to server.js

--- server.js
+++ server.js
@@ -2,3 +2,14 @@ var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
+
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
