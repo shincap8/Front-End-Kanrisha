85b58f21f59ef85ae4b76680b7879adc6a005ad9
Sara Hincapie <shincap8@gmail.com>
2020-06-16 15:53:52 -0500

    change path in server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ app.listen(process.env.PORT || 8080);
 
 if (process.env.NODE_ENV === 'production') {
     // Exprees will serve up production assets
-    app.use(express.static('client/build'));
+    app.use(express.static('public'));
 
     // Express serve up index.html file if it doesn't recognize route
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
     });
 }
