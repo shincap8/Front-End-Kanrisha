cc492263b524f750f8ab6506c53aa46377ea8a4c
Sara Hincapie <shincap8@gmail.com>
2020-06-17 08:04:24 -0500

    Let's see if this works

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(require('./routes/advances'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
 
 module.exports = app;
