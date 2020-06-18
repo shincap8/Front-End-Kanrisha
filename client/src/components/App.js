73f90b2f6f7ebfe075e1c8db3903715c9639ec4b
Sara Hincapie <shincap8@gmail.com>
2020-06-17 19:31:00 -0500

    Goback

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import { Router, Route } from 'react-router';
+import { Router, Switch, Route } from 'react-router-dom';
 import history from '../history';
 import { Login } from '../views/Login';
 import { Home } from '../views/Home';
-//import { Layout } from './Layout';
+import { Layout } from './Layout';
 import { NewProject } from '../views/NewProject';
 import { ProjectPage } from '../views/ProjectPage';
 import {NewTask} from '../views/NewTask';
 import {TaskPage} from '../views/TaskPage';
 import {AllProjects} from '../views/AllProjects';
 import { NotFound } from '../views/Page404';
-window.router = history;
 
 function App() {
   return (
     <Router history={history}>
+      <Switch>
         <Route exact path="/" component={Login} />
         <Route exact path="/NotFound" component={NotFound} />
-        {/*<Layout>*/}
+        <Layout>
           <Route exact path="/Home" component={Home} />
           <Route exact path="/AllProjects" component={AllProjects} />
           <Route exact path="/NewProject" component={NewProject} />
           <Route exact path="/ProjectPage" component={ProjectPage} />
           <Route exact path="/NewTask" component={NewTask} />
           <Route exact path="/TaskPage" component={TaskPage} />
-        {/*</Layout>*/}
+        </Layout>
+      </Switch>
     </Router>
   );
 }
