b4219c7a737fb04ecab05c2f158216e5a54d4a4c
shincap8 <43832784+shincap8@users.noreply.github.com>
2020-08-17 20:52:08 -0500

    Update NavBar to display celia logo

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../img/Logo.png';
+import logo from '../img/Logob.png';
 import history from '../history';
 
 export class NavBar extends React.Component {
